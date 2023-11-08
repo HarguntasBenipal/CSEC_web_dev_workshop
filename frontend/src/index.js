497c50d410c4d06af471ff37b0cc164a3f5991c3
Zacky <89254577+ZayLinAung@users.noreply.github.com>
2023-11-07 19:40:46 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let i = 1
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.querySelector('#add').addEventListener('click', () => add());
+  document.querySelector('#subtract').addEventListener('click', () => subtract());
+})
+
+function add(){
+  console.log('hi')
+  i = i + 1
+  document.querySelector('#value').innerHTML = i;
+}
+
+function subtract(){
+  console.log('hey')
+  i = i - 1
+  document.querySelector('#value').innerHTML = i;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
